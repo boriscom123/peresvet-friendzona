1dc4be5595d00022c0d17771c9da1c374da244c7
Борис Полянский <boriscom123@yandex.ru>
2021-07-20 21:38:34 +0300

    Move to GH from PHPStorm

--- wp-content/themes/pv-friendzone/assets/script/index.js
+++ wp-content/themes/pv-friendzone/assets/script/index.js
@@ -1,1 +1,14 @@
 console.log('blank');
+// прокрутка
+function scroll(){
+    let scrollHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
+    let totalSrcoll = scrollHeight - window.innerHeight;
+    let scrollStep = totalSrcoll / 100;
+    scrollBarEl[0].children[0].style.width = (window.pageYOffset / scrollStep) +'%';
+}
+let scrollBarEl = document.getElementsByClassName('header-progress-bar');
+document.addEventListener('scroll',  function(){scroll(this)});
